913cdc4f0955b427cf2b536713d08c7098ae7917
hellowindos <2409375386@qq.com>
2024-03-05 21:59:02 +0800

    Site updated: 2024-03-05 21:59:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/02/2024-3-2日记/","2024/03/02/祝两会顺利召开！/","2024/03/02/第一个文章/"];function toRandomPost(){
+var posts=["2024/03/02/2024-3-2日记/","2024/03/02/祝两会顺利召开！/","2024/03/02/第一个文章/","2024/03/05/体育课2024-3-5/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
